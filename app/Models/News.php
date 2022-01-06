6c84d37deadc0ea955cf94a82586ca8a80573c29
Tobias Hultqvist <hultqvist.tobias@gmail.com>
2022-01-06 19:58:23 +0100

    Added news model

---
+++ app/Models/News.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Orchid\Screen\AsSource;
+
+class News extends Model
+{
+    use HasFactory;
+    use AsSource;
+
+    protected $fillable = [
+        'status',
+        'fillable',
+        'image',
+        'content',
+        'featured',
+        'category',
+        'author',
+        'published_at'
+    ]
+}
