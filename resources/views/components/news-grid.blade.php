10821483b4f20e9a232c66b8002c63b7f31ba4d6
Tobias Hultqvist <hultqvist.tobias@gmail.com>
2022-01-11 20:03:01 +0100

    Renamed news.blade.php to news-card...

--- resources/views/components/news-grid.blade.php
+++ resources/views/components/news-grid.blade.php
@@ -1,6 +1,6 @@
 <div class="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-2">
     @foreach($news as $new)
-        @include('components.news', $new)
+        @include('components.news-card', $new)
     @endforeach
     
 
